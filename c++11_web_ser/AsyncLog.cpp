1ca01a9f94433feb4bcf7b95cb60a87803248409
zhuwenboa <https://zhuwenboa@github.com>
2020-03-13 21:42:44 +0800

    多线程日志

---
+++ c++11_web_ser/AsyncLog.cpp
@@ -0,0 +1,69 @@
+#include "AsyncLog.h"
+#include<thread>
+#include<unistd.h>
+#include<sys/types.h>
+#include<iostream>
+#include<fcntl.h>
+
+int Log::MAX_LOG = 100;
+
+int fd = open("sys_log.txt", O_RDWR | O_CREAT, 0666);
+
+
+Mutexlock Log_queue::mutex;
+Condition Log_queue::cond(mutex);
+std::queue<std::vector<std::string>> Log_queue::work_queue;
+int Log_queue::MAX_BACKEND_LEN = 20;
+int Log_queue::backend_buffer_len = 0;
+
+void Log::add_log(const std::string& mes)
+{
+    //如果该工作线程的日志数大于MAX_LOG，则将其添加到工作线程进行处理
+    if(buffer_len >= MAX_LOG)
+    {
+        Log_queue::append(log_buffer);
+        buffer_len = 0;
+        log_buffer.clear();
+    }
+    else
+    {
+        log_buffer.emplace_back(mes);
+        ++buffer_len;    
+    }
+}
+
+void Log_queue::append(std::vector<std::string> messages)
+{
+    mutex.lock();
+    work_queue.emplace(messages);
+    ++backend_buffer_len;
+    if(backend_buffer_len >= MAX_BACKEND_LEN)
+    {
+        mutex.unlock();
+        cond.notify();
+    }
+    else 
+        mutex.unlock();
+}
+
+void Log_queue::work()
+{
+    while(true)
+    {
+        mutex.lock();
+        while(backend_buffer_len < MAX_BACKEND_LEN)
+        {
+            cond.wait();
+        }
+        //将数据写入磁盘，并更新backend_buffer_len
+        for(int i = 0; i < backend_buffer_len; ++i)
+        {
+            for(auto a : work_queue.front())
+            {
+                write(fd, a.c_str(), a.size());
+            }
+        }                
+        backend_buffer_len = 0;
+        mutex.unlock();
+    }
+}
---
+++ c++11_web_ser/AsyncLog.h
@@ -0,0 +1,43 @@
+#ifndef ASYNCLOG_H
+#define ASYNCLOG_H
+#include<vector>
+#include<string.h>
+#include<string>
+#include<queue>
+#include "Mutexlock.h"
+
+class Log_queue;
+
+class Log
+{
+public:  
+    Log() : buffer_len(0) {}
+    void add_log(const std::string&);
+
+private: 
+
+    std::vector<std::string> log_buffer;     
+    int buffer_len;
+    static int MAX_LOG;
+};
+
+class Log_queue
+{
+public:   
+    Log_queue() {}
+    //删除拷贝构造和拷贝赋值函数
+    Log_queue(const Log_queue&)  = delete;
+    Log_queue operator = (const Log_queue&)  = delete;
+
+    static void append(std::vector<std::string>);    
+    static void work();
+
+private:  
+    static std::queue<std::vector<std::string>> work_queue;
+    static Mutexlock mutex;
+    static Condition cond;
+    static int backend_buffer_len;
+    static int MAX_BACKEND_LEN;
+};
+
+#endif
